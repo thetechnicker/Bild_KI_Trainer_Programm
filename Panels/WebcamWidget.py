c8c3818755da316b70187ac6f890cea18eeaba58
thetechnicker <https.LocalHorst@gmx.de>
2023-05-13 18:21:38 +0200

    ?

--- Panels/WebcamWidget.py
+++ Panels/WebcamWidget.py
@@ -2,6 +2,7 @@ from PyQt5.QtMultimedia import QCamera, QCameraInfo
 from PyQt5.QtMultimediaWidgets import QCameraViewfinder
 from PyQt5.QtWidgets import QApplication
 from PyQt5 import QtWidgets
+from PyQt5 import QtCore
 import sys
 
 class CameraWidget(QtWidgets.QWidget):
@@ -12,13 +13,23 @@ class CameraWidget(QtWidgets.QWidget):
 
         self.viewfinder = QCameraViewfinder()
         self.camera.setViewfinder(self.viewfinder)
+        print(self.camera.focus().maximumDigitalZoom())
+        self.zoom_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.zoom_slider.setRange(0, int(self.camera.focus().maximumDigitalZoom() * 100))
+        self.zoom_slider.setValue(int(self.camera.focus().digitalZoom() * 100))
+        self.zoom_slider.valueChanged.connect(self.update_zoom)
 
         layout = QtWidgets.QVBoxLayout(self)
         layout.addWidget(self.viewfinder)
+        layout.addWidget(self.zoom_slider)
 
-        
+
 
         self.camera.start()
+
+    def update_zoom(self, value):
+        zoom_value = value / 100.0
+        self.camera.focus().zoomTo(zoom_value, zoom_value)
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
