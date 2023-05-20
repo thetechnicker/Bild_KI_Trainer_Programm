96dc0a4694e7f665cc2f0b30d1686c1f0c4e799b
thetechnicker <https.LocalHorst@gmx.de>
2023-05-20 17:11:25 +0200

    finished treeview cleanup

--- Panels/TreeviewPanel.py
+++ Panels/TreeviewPanel.py
@@ -1,5 +1,6 @@
 import os
 import json
+import sys
 from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtCore import Qt
 
@@ -8,16 +9,38 @@ class EditableStandardItem(QtGui.QStandardItem):
         super().__init__(*args, **kwargs)
         self.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
 
+    def is_folder(self):
+        # Check if the second column contains the string 'folder'
+        if 'folder' in self.model().item(self.row(), 1).text():
+            return True
+        return False
 
-    def is_file(self):
-        # Check if the second column contains the string 'file'
-        if 'file' in self.model().item(self.row(), 1).text():
+    def is_val(self):
+        if 'val' in self.model().item(self.row(), 1).text():
+            return True
+        return False
+    
+    def is_non(self):
+        # Check if the second column contains the string 'non'
+        if 'non' in self.model().item(self.row(), 1).text():
             return True
         return False
 
     def is_cnn(self):
-        # Check if the second column contains the string 'value'
+        # Check if the second column contains the string 'cnn'
         if 'cnn' in self.model().item(self.row(), 1).text():
+            return True
+        return False
+
+    def is_class(self):
+        # Check if the second column contains the string 'class'
+        if 'class' in self.model().item(self.row(), 1).text():
+            return True
+        return False
+
+    def is_img(self):
+        # Check if the second column contains the string 'img'
+        if 'img' in self.model().item(self.row(), 1).text():
             return True
         return False
 
@@ -27,9 +50,7 @@ class EditableStandardItem(QtGui.QStandardItem):
             'type': self.model().item(self.row(), 1).text()
         }
 
-from PyQt5 import QtWidgets
-
-class ÁddDialog(QtWidgets.QDialog):
+class AddDialog(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
         layout = QtWidgets.QVBoxLayout(self)
@@ -49,7 +70,7 @@ class ÁddDialog(QtWidgets.QDialog):
         button_box.accepted.connect(self.accept)
         button_box.rejected.connect(self.reject)
         layout.addWidget(button_box)
-    
+
     def get_data(self):
         item_type = self.type_select.currentText()
         item_name = self.name_input.text()
@@ -58,58 +79,60 @@ class ÁddDialog(QtWidgets.QDialog):
 class TreeviewPanel(QtWidgets.QWidget):
     def __init__(self, file_path=None):
         super().__init__()
-        self.callback=None
-
+        
         self.file_path = file_path
 
         layout = QtWidgets.QVBoxLayout(self)
 
-
         self.tree_view = QtWidgets.QTreeView()
+        
         self.model = QtGui.QStandardItemModel(self)
+        
         self.model.setColumnCount(2)
+        
         self.model.setHeaderData(0, Qt.Horizontal,"Name")
+        
         self.model.setHeaderData(1, Qt.Horizontal, "Type")
+        
         self.tree_view.setModel(self.model)
+        
         self.tree_view.hideColumn(1)
+        
         layout.addWidget(self.tree_view)
-
         button_layout = QtWidgets.QHBoxLayout()
+        
         add_button = QtWidgets.QPushButton('Add')
+        
         add_button.clicked.connect(self.add_item)
+        
         button_layout.addWidget(add_button)
 
         remove_button = QtWidgets.QPushButton('Remove')
+        
         remove_button.clicked.connect(self.remove_item)
+        
         button_layout.addWidget(remove_button)
+        
         layout.addLayout(button_layout)
-
+        self.callback=None
         if self.file_path and self.file_path.endswith('.json'):
             self.load_json(self.file_path)
-        self.tree_view.doubleClicked.connect(self.on_double_click)
-
-    def setJson(self, file):
-        self.file_path=file
-        self.load_json(file)
-
 
     def on_double_click(self, index):
         item = self.model.itemFromIndex(index)
         if self.callback:
             self.callback(item)
-    
     def set_callback(self, callback):
-        self.callback = callback
-            
-
+        self.callback=callback
 
     def remove_item(self):
         selected_indexes = self.tree_view.selectedIndexes()
 
         if selected_indexes:
             index = selected_indexes[0]
-            self.model.removeRow(index.row(), index.parent())
-
+            item = self.model.itemFromIndex(index)
+            if not item.is_non() and not item.is_folder():
+                self.model.removeRow(index.row(), index.parent())
 
     def select_file(self):
         file_dialog = QtWidgets.QFileDialog()
@@ -141,7 +164,6 @@ class TreeviewPanel(QtWidgets.QWidget):
         root = self.model.invisibleRootItem()
         return search_children(root)
 
-
     def load_json(self, file_path):
         fail=False
         data=None
@@ -165,32 +187,78 @@ class TreeviewPanel(QtWidgets.QWidget):
         if fail:
             with open(file_path, "w") as f:
                 json.dump(data,f)
-        
 
-    def add_items(self, parent_item, data):
-        if isinstance(data, dict):
-            for key, value in data.items():
-                if not key=="label":
-                    name_item = EditableStandardItem(key)
-                    type_item = EditableStandardItem('folder')
-                    parent_item.appendRow([name_item, type_item])
-                    self.add_items(name_item, value)
-        elif isinstance(data, list):
-            for index, value in enumerate(data):
-                label=""
-                if isinstance(value, dict):
-                    if value["label"]:
-                        label= value["label"]
+    def add_items(self, parent, data):
+        #print(data)
+        for key, value in data.items():
+            #print(f"{key}: {value}")
+            if key=="Images":
+                parent_name="Images"
+                self.model.appendRow([EditableStandardItem(parent_name),EditableStandardItem("folder")])
+                #print(value)
+                for i in value:
+                    #print(i)
+                    parent = self.get_item_by_name("Images")
+                    name_item = EditableStandardItem(i["label"])
+                    file_item = EditableStandardItem("File")
+                    name_item.appendRow([file_item,EditableStandardItem("non")])
+                    if "Yolo" in i:
+                        yolo_item = EditableStandardItem("Yolo")
+                        gx_item = EditableStandardItem("gx")
+                        gy_item = EditableStandardItem("gy")
+                        x_item = EditableStandardItem("x")
+                        y_item = EditableStandardItem("y")
+                        c_item = EditableStandardItem("Class")
+                        Yolo=i["Yolo"]
+
+                        file_item.appendRow([EditableStandardItem(i["File"]),EditableStandardItem("val")])
+                        gx_item.appendRow([EditableStandardItem(str(Yolo["gx"])),EditableStandardItem("val")])
+                        gy_item.appendRow([EditableStandardItem(str(Yolo["gy"])),EditableStandardItem("val")])
+                        x_item.appendRow([EditableStandardItem(str(Yolo["x"])),EditableStandardItem("val")])
+                        y_item.appendRow([EditableStandardItem(str(Yolo["y"])),EditableStandardItem("val")])
+                        c_item.appendRow([EditableStandardItem(str(Yolo["Class"])),EditableStandardItem("val")])
+
+                        yolo_item.appendRow([gx_item,EditableStandardItem("non")])
+                        yolo_item.appendRow([gy_item,EditableStandardItem("non")])
+                        yolo_item.appendRow([x_item,EditableStandardItem("non")])
+                        yolo_item.appendRow([y_item,EditableStandardItem("non")])
+                        yolo_item.appendRow([c_item,EditableStandardItem("non")])
+                        name_item.appendRow([yolo_item,EditableStandardItem("non")])
+
+                    parent.appendRow([name_item,EditableStandardItem("img")])
+            elif key == "Neuronale Netze":
+                parent_name = "Neuronale Netze"
+                self.model.appendRow([EditableStandardItem(parent_name),EditableStandardItem("folder")])
+                name = "cnn"
+                for i in value:
+                    #print(i)
+
+                    parent = self.get_item_by_name(parent_name)
+
+                    name_item = EditableStandardItem(i)
+                    type_item = EditableStandardItem(name)
+
+                    if parent:
+                        parent.appendRow([name_item, type_item])
                     else:
-                        label=str(index + 1)
-                name_item = EditableStandardItem(label)
-                type_item = EditableStandardItem('folder')
-                parent_item.appendRow([name_item, type_item])
-                self.add_items(name_item, value)
-        else:
-            name_item = EditableStandardItem(str(data))
-            type_item = EditableStandardItem('file')
-            parent_item.appendRow([name_item, type_item])
+                        self.model.appendRow([name_item, type_item])
+            elif key == "Classes":
+                parent_name = "Classes"
+                self.model.appendRow([EditableStandardItem(parent_name),EditableStandardItem("folder")])
+                name = "class"
+                for i in value:
+                    #print(i)
+
+                    parent = self.get_item_by_name(parent_name)
+    
+                    name_item = EditableStandardItem(i)
+                    type_item = EditableStandardItem(name)
+    
+                    if parent:
+                        parent.appendRow([name_item, type_item])
+                    else:
+                        self.model.appendRow([name_item, type_item])
+                   
 
 
     def find_item(self, path_parts):
@@ -203,41 +271,65 @@ class TreeviewPanel(QtWidgets.QWidget):
                     break
         return parent_item
     
+    def get_structure(self):
+        root=self.model.invisibleRootItem()
+        structure={}
+        for i in range(root.rowCount()):
+            item=root.child(i)
+            if item.text()=="Classes":
+                d=[]
+                if item.hasChildren():
+                    for j in range(item.rowCount()):
+                        d.append(item.child(i).text())
+                structure["Classes"]=d
+            elif item.text()=="Neuronale Netze":
+                d=[]
+                if item.hasChildren():
+                    for j in range(item.rowCount()):
+                        d.append(item.child(i).text())
+                structure["Neuronale Netze"]=d
+            elif item.text()=="Images":
+                d=[]
+                if item.hasChildren():
+                    for i in range(item.rowCount()):
+                        item=item.child(i)
+                        img_data={}
+                        img_data["label"]=item.text()
+                        if item.hasChildren():
+                            for j in range(item.rowCount()):
+                                print(item.child(j).text())
+                                if item.child(j).text()=="File":
+                                    img_data["File"]=item.child(j).child(0).text()
+                                elif item.child(j).text()=="Yolo":
+                                    
+                                    Yolo_data={}
+                                    item=item.child(j)
+                                    if item.hasChildren():
+                                        for k in range(item.rowCount()):
+                                            Yolo_data[item.child(k).text()]=int(item.child(k).child(0).text())
+                                    img_data["Yolo"]=Yolo_data
+                                else:
+                                    img_data["rest"]="Error"
+                        d.append(img_data)
+                        
+                structure["Images"]=d
+        return structure
 
 
-    def get_tree_structure(root_item):
-        def get_structure(item):
-            if item.hasChildren():
-                structure = []
-                for row in range(item.rowCount()):
-                    child_item = item.child(row)
-                    value = get_structure(child_item)
-                    structure.append(value)
-                return {item.text(): structure}
-            else:
-                return item.text()
-    
-        tree_structure = {}
-        for row in range(root_item.rowCount()):
-            item = root_item.child(row)
-            key = item.text()
-            value = get_structure(item)[key]
-            tree_structure[key] = value
-    
-        return tree_structure
-    
     def saveJson(self):
+        d=self.get_structure()
+        #print(d)
         with open(self.file_path, "w") as f:
-            d=self.get_item_by_name()
             json.dump(d,f)
 
     def add_item(self):
-        dialog = ÁddDialog(self)
+        dialog = AddDialog(self)
 
         if dialog.exec_() == QtWidgets.QDialog.Accepted:
             item_type, item_name = dialog.get_data()
 
             name = ""
+            parent_name=""
             if item_type == "Image":
                 parent = self.get_item_by_name("Images")
                 name_item = EditableStandardItem(item_name)
@@ -249,52 +341,44 @@ class TreeviewPanel(QtWidgets.QWidget):
                 y_item = EditableStandardItem("y")
                 c_item = EditableStandardItem("c")
 
-                value=EditableStandardItem()
-                file_item.appendRow([value])
-                value=EditableStandardItem()
-                gx_item.appendRow([value])
-                value=EditableStandardItem()
-                gy_item.appendRow([value])
-                value=EditableStandardItem()
-                x_item.appendRow([value])
-                value=EditableStandardItem()
-                y_item.appendRow([value])
-                value=EditableStandardItem()
-                c_item.appendRow([value])
-                
+                file_item.appendRow([EditableStandardItem("0"),EditableStandardItem("val")])
+                gx_item.appendRow([EditableStandardItem("0"),EditableStandardItem("val")])
+                gy_item.appendRow([EditableStandardItem("0"),EditableStandardItem("val")])
+                x_item.appendRow([EditableStandardItem("0"),EditableStandardItem("val")])
+                y_item.appendRow([EditableStandardItem("0"),EditableStandardItem("val")])
+                c_item.appendRow([EditableStandardItem("0"),EditableStandardItem("val")])
 
-                yolo_item.appendRow([gx_item])
-                yolo_item.appendRow([gy_item])
-                yolo_item.appendRow([x_item])
-                yolo_item.appendRow([y_item])
-                yolo_item.appendRow([c_item])
+                yolo_item.appendRow([gx_item,EditableStandardItem("non")])
+                yolo_item.appendRow([gy_item,EditableStandardItem("non")])
+                yolo_item.appendRow([x_item,EditableStandardItem("non")])
+                yolo_item.appendRow([y_item,EditableStandardItem("non")])
+                yolo_item.appendRow([c_item,EditableStandardItem("non")])
 
-                name_item.appendRow([file_item])
-                name_item.appendRow([yolo_item])
+                name_item.appendRow([file_item,EditableStandardItem("non")])
+                name_item.appendRow([yolo_item,EditableStandardItem("non")])
 
-                self.model.appendRow([name_item])
+                parent.appendRow([name_item,EditableStandardItem("img")])
 
             elif item_type == "Neural Net":
-                name = "Neuronale Netze"
+                parent_name = "Neuronale Netze"
+                name = "cnn"
             elif item_type == "Class":
-                name = "classes"
-            parent = self.get_item_by_name(name)
+                parent_name = "Classes"
+                name = "class"
+            if not item_type=="Image":
+                parent = self.get_item_by_name(parent_name)
 
-            if item_type != "Image":
                 name_item = EditableStandardItem(item_name)
-                type_item = EditableStandardItem(item_type)     
+                type_item = EditableStandardItem(name)
 
                 if parent:
                     parent.appendRow([name_item, type_item])
                 else:
                     self.model.appendRow([name_item, type_item])
-
     
-    
-
-
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
     tree_view_panel = TreeviewPanel("C:/Users/lucas/Documents/Python/GUI/Bild_KI_Trainer_Programm/test.json")
     tree_view_panel.show()
     app.exec_()
+    tree_view_panel.saveJson()
