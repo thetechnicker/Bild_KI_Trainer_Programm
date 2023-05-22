137933ea4d037f536b12cd50a549af40a143dbb7
thetechnicker <https.LocalHorst@gmx.de>
2023-05-22 15:13:26 +0200

    CameraSettings

--- Panels/CameraViewDialog.py
+++ Panels/CameraViewDialog.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtWidgets import QDialog, QLabel, QGridLayout, QLineEdit, QPushButton
 
-if not (__name__=="__main__") and True:
+if not (__name__=="__main__") and False:
     from .WebcamWidget import WebcamWidget
 else:
     from WebcamWidget import WebcamWidget
@@ -20,12 +20,19 @@ class CameraViewDialog(QDialog):
         # Add input fields for x and y coordinates of rectangle
         self.xInput = QLineEdit(self)
         self.yInput = QLineEdit(self)
+        
+        self.wInput = QLineEdit(self)
+        self.hInput = QLineEdit(self)
 
         self.gxInput = QLineEdit(self)
         self.gyInput = QLineEdit(self)
 
-        self.xInput.setText("1")
-        self.yInput.setText("1")
+        self.xInput.setText("0")
+        self.yInput.setText("0")
+
+        self.wInput.setText("")
+        self.hInput.setText("")
+
         self.gxInput.setText("10")
         self.gyInput.setText("10")
         
@@ -35,6 +42,12 @@ class CameraViewDialog(QDialog):
 
         self.yInput.setValidator(QtGui.QIntValidator())
         self.yInput.textChanged.connect(self.updateGrid)
+        
+        self.wInput.setValidator(QtGui.QIntValidator())
+        self.wInput.textChanged.connect(self.updateGrid)
+
+        self.hInput.setValidator(QtGui.QIntValidator())
+        self.hInput.textChanged.connect(self.updateGrid)
 
         self.gxInput.setValidator(QtGui.QIntValidator())
         self.gxInput.textChanged.connect(self.updateGrid)
@@ -47,16 +60,21 @@ class CameraViewDialog(QDialog):
         gridLayout.addWidget(QLabel("Y:"), 1, 0)
         gridLayout.addWidget(self.yInput, 1, 1)
         
-        gridLayout.addWidget(QLabel("GX:"), 2, 0)
-        gridLayout.addWidget(self.gxInput, 2, 1)
-        gridLayout.addWidget(QLabel("GY:"), 3, 0)
-        gridLayout.addWidget(self.gyInput, 3, 1)
+        gridLayout.addWidget(QLabel("Width:"), 2, 0)
+        gridLayout.addWidget(self.wInput, 2, 1)
+        gridLayout.addWidget(QLabel("Height:"), 3, 0)
+        gridLayout.addWidget(self.hInput, 3, 1)
+        
+        gridLayout.addWidget(QLabel("Horizontal Splitcount:"), 4, 0)
+        gridLayout.addWidget(self.gxInput, 4, 1)
+        gridLayout.addWidget(QLabel("Vertikal Splitcount:"), 5, 0)
+        gridLayout.addWidget(self.gyInput, 5, 1)
 
         # Add save and cancel buttons
         saveButton = QPushButton('Save', self)
         cancelButton = QPushButton('Cancel', self)
-        gridLayout.addWidget(saveButton, 4, 0)
-        gridLayout.addWidget(cancelButton, 4, 1)
+        gridLayout.addWidget(saveButton, 6, 0)
+        gridLayout.addWidget(cancelButton, 6, 1)
         layout=QtWidgets.QHBoxLayout(self)
         layout.addWidget(self.cam)
         layout.addLayout(gridLayout)
@@ -69,6 +87,8 @@ class CameraViewDialog(QDialog):
         try:
             x = int(self.xInput.text())
             y = int(self.yInput.text())
+            w = int(self.wInput.text())
+            h = int(self.hInput.text())
             gx = int(self.gxInput.text())
             gy = int(self.gyInput.text())
             self.cam.setGrid(x=x,y=y,horizontal_lines=gx,vertical_lines=gy)
