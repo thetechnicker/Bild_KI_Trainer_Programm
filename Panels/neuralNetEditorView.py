581a549e937615f7d8d358bffcc603fec7aaa596
thetechnicker <https.LocalHorst@gmx.de>
2023-08-13 00:02:28 +0200

    works

--- Panels/neuralNetEditorView.py
+++ Panels/neuralNetEditorView.py
@@ -1,4 +1,65 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
+
+class TabEditor(QtWidgets.QLineEdit):
+    def __init__(self, index, parent=None):
+        super().__init__(parent)
+        self.index = index
+        self.parent = parent
+
+        # Set the initial text and geometry of the editor
+        self.setText(parent.tabText(index))
+        self.setGeometry(self.getEditorRect())
+
+        # Connect signals
+        self.editingFinished.connect(self.finishEditing)
+
+    def getEditorRect(self):
+        # Get the rectangle of the tab at the given index
+        rect = self.parent.tabBar().tabRect(self.index)
+        # Map the rectangle to the coordinates of the tab widget
+        rect.moveTopLeft(self.parent.tabBar().mapTo(self.parent, rect.topLeft()))
+        return rect
+
+    def finishEditing(self):
+        # Set the new text of the tab and hide the editor
+        self.parent.setTabText(self.index, self.text())
+        project_folder=self.parent.parent.project_folder
+        print(project_folder)
+        self.hide()
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Escape:
+            # Discard changes and hide the editor
+            self.hide()
+        else:
+            super().keyPressEvent(event)
+
+class MyTabWidget(QtWidgets.QTabWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.parent=parent
+        self.editor = None
+
+    def setCallback(self, callback):
+        self.callback=callback
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.RightButton:
+            index = self.tabBar().tabAt(event.pos())
+            if index != -1:
+                # Create and show the editor
+                if self.editor is not None:
+                    self.editor.deleteLater()
+                self.editor = TabEditor(index, self)
+                self.editor.show()
+                self.editor.setFocus()
+            elif self.editor:
+                self.editor.hide()
+        else:
+            if self.editor:
+                self.editor.hide()
+                    
+        super().mousePressEvent(event)
 
 if __name__=="__main__":
     from neuralNetEditor import NeuralNetEditor
@@ -10,7 +71,7 @@ class NeuralNetTabView(QtWidgets.QWidget):
         super().__init__()
 
         # Create the tab widget
-        self.tab_widget = QtWidgets.QTabWidget()
+        self.tab_widget = MyTabWidget(self)
         self.project_folder = None
 
         # Lay out the UI elements
